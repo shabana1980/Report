c96e207ad6086260a1cb39bd5aae3dc7e20d7442
Shabana Tasneem <Shabana1980@gmail.com>
2021-03-22 11:08:44 +0300

    changes to Main

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ from  ReportWriter import ReportFile
 
 if __name__ == '__main__':
     print("Hello World!")
-    print("I removed the line")
     P1 = ReportDataProvider("Shabana", 41)
    # P1.printData()
     ReportFile.PrepareReport(P1)   
