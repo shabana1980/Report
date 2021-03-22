8881112f0144932b303be047099d4d032f1d4267
Shabana Tasneem <Shabana1980@gmail.com>
2021-03-22 11:02:20 +0300

    Added a new line in Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ from ReportData import ReportDataProvider
 from  ReportWriter import ReportFile
 
 if __name__ == '__main__':
-    print("Hello World! This is my first report Application")
+    print("Hello World!")
+    print("I have added a new line!!")
     P1 = ReportDataProvider("Shabana", 41)
    # P1.printData()
     ReportFile.PrepareReport(P1)   
