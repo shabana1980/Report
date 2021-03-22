8f7eb3c64487152c793d123ec33bca21e64d1464
shabana1980 <61321416+shabana1980@users.noreply.github.com>
2021-03-22 11:06:51 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from  ReportWriter import ReportFile
 
 if __name__ == '__main__':
     print("Hello World!")
-    print("I have added a new line!!")
+    print("I removed the line")
     P1 = ReportDataProvider("Shabana", 41)
    # P1.printData()
     ReportFile.PrepareReport(P1)   
