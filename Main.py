dd0cc6275e3e427ee723aeab2d992887ce5c0faf
Shabana Tasneem <Shabana1980@gmail.com>
2021-03-22 10:57:30 +0300

    changes made to Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from ReportData import ReportDataProvider
 from  ReportWriter import ReportFile
 
 if __name__ == '__main__':
-    print("Hello World! This is my first report")
+    print("Hello World! This is my first report Application")
     P1 = ReportDataProvider("Shabana", 41)
    # P1.printData()
     ReportFile.PrepareReport(P1)   
